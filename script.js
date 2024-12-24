d7bf36f361fa9a4e59f31c8188d9b7edd19cea9e
Rajdip Dutta <157410013+rajdip29@users.noreply.github.com>
2024-12-24 21:06:33 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,39 @@
+var crsr = document.querySelector("#cursor");
+var crsrBlur = document.querySelector("#cursor-blur");
+
+document.addEventListener("mousemove",function(dets){
+    crsr.style.left = dets.x+"px"
+    crsr.style.top = dets.y+"px"
+     crsrBlur.style.left = dets.x - 150 +"px"
+    crsrBlur.style.top = dets.y - 150 +"px"
+})
+
+
+
+
+gsap.to("#nav",{
+    backgroundColor : "#000" ,
+    height: "110px",
+    duration:0.5,
+    scrollTrigger:{
+        trigger: "#nav",
+        scroller:"body",
+        markers: true,
+        start : "top -10%",
+        end : "top -11%",
+        scrub: 2
+    }
+    
+})
+
+gsap.to("#main", {
+    backgroundColor : "#000",
+    scrollTrigger: {
+        trigger: "#main",
+        scroller : "body",
+        markers: true,
+        start: "top -25%",
+        end : "top -70%",
+        scrub : 2
+    }
+})
