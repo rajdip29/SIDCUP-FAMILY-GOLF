6e7d0501e19a935414c5a8f5269f440424d34242
Rajdip Dutta <157410013+rajdip29@users.noreply.github.com>
2024-12-25 23:57:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ gsap.to("#nav",{
     scrollTrigger:{
         trigger: "#nav",
         scroller:"body",
-        markers: true,
+        // markers: true,
         start : "top -10%",
         end : "top -11%",
         scrub: 2
@@ -31,7 +31,7 @@ gsap.to("#main", {
     scrollTrigger: {
         trigger: "#main",
         scroller : "body",
-        markers: true,
+        // markers: true,
         start: "top -25%",
         end : "top -70%",
         scrub : 2
